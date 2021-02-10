4fea2cd494148620af95aa7f5595e505a183d5ef
whuandra <50607920+whuandra@users.noreply.github.com>
2021-02-09 20:32:52 -0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "wino_test_s3_2921" {
 bucket = "wino-test-s3-2921-tues"
-  
+  }
+
+resource "aws_s3_bucket_policy" "wino_test_s3_pol" {
+  bucket = aws_s3_bucket.wino_test_s3_2921.id
   policy = <<POLICY
 {
 "Version": "2012-10-17",
