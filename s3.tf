f21c0b60a23401d83f7e021166c19d82d0f15004
whuandra <50607920+whuandra@users.noreply.github.com>
2021-02-09 20:57:01 -0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,11 +4,14 @@ bucket = "wino-test-s3-2921-tues"
 
 resource "aws_s3_bucket_policy" "wino_test_s3_pol" {
   bucket = aws_s3_bucket.wino_test_s3_2921.id
+  policy_format = "s3"
   policy = <<POLICY
 {
+"Version": "2008-10-17",
+"Id": "MYBUCKETPOLICY",
 "Statement": [
 {
-"sid": "AddPerm",
+"sid": "IPAllow",
 "Effect": "Allow",
 "Action": [
 "s3:ListBucket",
@@ -19,9 +22,7 @@ resource "aws_s3_bucket_policy" "wino_test_s3_pol" {
 "s3:DeleteObject"
 ],
 "Resource": "arn:aws:s3:::wino-test-s3-2921-tues/*",
-"Principal": {
-"AWS": "arn:aws:iam::590661738553:user/scott"
-  }
+"Principal": "*"
 }
 ]
 }
