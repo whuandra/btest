d749d59ef9dd1df099704dab3e916646ff0b74ae
whuandra <50607920+whuandra@users.noreply.github.com>
2021-02-09 16:51:28 -0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,29 @@
 resource "aws_s3_bucket" "wino_test_s3_2921" {
 bucket = "wino-test-s3-2921-tues"
+  
+  policy = <<POLICY
+{
+"Version": "2012-10-17",
+"Id": "MYBUCKETPOLICY",
+"Statement": [
+{
+"sid": "UserAllow",
+"Effect": "Allow",
+"Action": [
+"s3:ListBucket",
+"s3:GetBucketLocation",
+"s3:PutObject",
+"s3:PutObjectAcl",
+"s3:Get*",
+"s3:DeleteObject"
+],
+"Resource": "arn:aws:s3:::wino-test-s3-2921-tues/*",
+"Principal": {
+"AWS": "arn:aws:iam::590661738553:user/scott"
 }
+}
+]
+}
+}
+
+
